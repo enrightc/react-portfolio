b7ad60a4e27b7bd559aee23c617a649f64ac2482
Charles Enright <c.enright@me.com>
2025-01-03 16:50:29 +0000

    Conditional Rendering exercis

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 import StatefulGreeting from "./components/StatefulGreetingWithPrevState";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I am a stateful class component" />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={false}/>
     </div>
   );
 }
---
+++ my-app/src/components/snippets.js
@@ -0,0 +1,62 @@
+// import React from 'react'
+
+// function snippets() {
+//   return (
+//     <div>snippets</div>
+//   )
+// }
+
+
+
+
+
+
+// export default snippets
+
+// import React from 'react'
+
+// const snippets = () => {
+//   return (
+//     <div>snippets</div>
+//   )
+// }
+
+// export default snippets
+
+
+
+
+
+
+// import React, { Component } from 'react'
+
+// export class snippets extends Component {
+//   render() {
+//     return (
+//       <div>snippets</div>
+//     )
+//   }
+// }
+
+// export default snippets
+
+
+
+
+
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+
+
+
+// console.log(first)
+
+// console.log('first', first)
