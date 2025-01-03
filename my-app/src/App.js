3fcd94b4ef36f8457297b9294f066454f415037c
Charles Enright <c.enright@me.com>
2025-01-03 17:54:02 +0000

    Methods as Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,15 +6,13 @@ import EventsClass from "./components/EventsClass";
 import EventsFunctional from "./components/EventsFunctional";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
-
+import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
-  return (
-    <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={false}/>
-    </div>
-  );
+  return <div className="App">
+      <MethodsAsPropsParent />
+  </div>;
 }
 
 export default App;
